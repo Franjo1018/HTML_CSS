41d1c3a769a143c70e51aaf376573a01675cbb7d
CPU <CPU@DESKTOP-4MBU90P>
2022-09-28 21:19:49 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/InicioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML_CSS.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult PagInicio()
+        {
+            return View();
+        }
+    }
+}
